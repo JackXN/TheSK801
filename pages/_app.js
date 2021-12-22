a38b3ccbbec26e5494cb9ae5bb63748ad8fa100e
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-12-21 20:14:56 -0700

    initial file setup and theme setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react';
+import {extendTheme} from '@chakra-ui/react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const theme = extendTheme();
+  
+return (
+  <ChakraProvider theme={theme}>
+ <Component {...pageProps} />
+ </ChakraProvider>
+)
 }
 
 export default MyApp
